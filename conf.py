7965b15150ead9c83ae07582334af4562e9d9d81
Hadi Mottale <HadiMottale@gmail.com>
2026-06-19 12:59:53 +0330

    feat(community): add chapter 5 on InnerSource
    - Added definition and core principles of InnerSource
    - Documented key benefits: improved discovery, collaboration, innovation, and code quality
    - Included InnerSource as a training ground for external open source contribution
    - Version bump to 0.0.5

--- conf.py
+++ conf.py
@@ -9,7 +9,7 @@ project = 'dpic'
 copyright = ' 2004-2048, Dataist'
 author = 'Hadi Mottale'
 version = '0.0'
-release = '0.0.4'
+release = '0.0.5'
 
 # -- General Configuration ---------------------------------------------------
 extensions = [
